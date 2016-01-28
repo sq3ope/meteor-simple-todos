485843ac2ab1525221a121031661776da459b705
Pawel Firlej <sq3ope@gmail.com>
2016-01-28 23:11:28 +0100

    Filter completed tasks

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,7 +4,17 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
     }
   });
 
@@ -24,6 +34,10 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
+    },
+
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
@@ -34,6 +48,7 @@ if (Meteor.isClient) {
         $set: {checked: ! this.checked}
       });
     },
+
     "click .delete": function () {
       Tasks.remove(this._id);
     }
